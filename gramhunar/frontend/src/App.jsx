748541a0ac32472236997a900da7a6cbf8249d6f
Taha <tahaalotwala@gmail.com>
2024-06-16 00:44:35 +0530

    create ui for schools

--- gramhunar/frontend/src/App.jsx
+++ gramhunar/frontend/src/App.jsx
@@ -3,8 +3,10 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./Components/Login";
 import Student from "./Components/Student";
 import Class from "./Components/Class";
+import Classes from "./Components/Classes";
 import AddActivity from "./Components/AddActivity";
 import Activities from "./Components/Activities";
+import Schools from "./Components/Schools";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,8 +15,10 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard/student/:id" element={<Student />} />
-        <Route path="/dashboard/class/:id" element={<Class />} />
+        <Route path="/schools" element={<Schools />} />
+        <Route path="/schools/:SchoolId/classes" element={<Classes />} />
+        <Route path="/schools/:SchoolId/classes/:id" element={<Class />} />
+        <Route path="/dashboard/students/:id" element={<Student />} />
         <Route path="/add-activity" element={<AddActivity />} />
         <Route path="/activities" element={<Activities />} />
       </Routes>
