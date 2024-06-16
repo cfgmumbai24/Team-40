525371b0c33528a134f82e0aa20e651eecec7dac
Taha <tahaalotwala@gmail.com>
2024-06-16 10:08:57 +0530

    password change

--- gramhunar/frontend/src/Components/Login.jsx
+++ gramhunar/frontend/src/Components/Login.jsx
@@ -65,7 +65,7 @@ const Login = () => {
                     }}
                     id="password"
                     name="password"
-                    type="text"
+                    type="password"
                     autoComplete="current-password"
                     className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     placeholder="Enter your password"
